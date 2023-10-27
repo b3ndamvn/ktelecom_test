2203ed6594161df4bf0a4c66c15cdc8fd379eed2
Nikita Benda <nikitabenda69@gmail.com>
2023-10-27 21:26:57 +0500

    create models

--- equipment/models.py
+++ equipment/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class EquipmentType(models.Model):
+    name = models.CharField(max_length=50)
+    mask = models.CharField(max_length=10, unique=True, validators=[
+        RegexValidator(
+            regex='^[NAaXZ]{10}$',
+            message='Неверный формат ввода',
+            code='invalid_mask'
+        ),
+    ])
+
+
+class Equipment(models.Model):
+    equipment_type = models.ForeignKey(EquipmentType, on_delete=models.CASCADE)
+    serial_number = models.CharField(max_length=10)
+    notice = models.TextField()
