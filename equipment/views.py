da1d9445f6b8ed8954853512eff847fd9cb345cb
Nikita Benda <nikitabenda69@gmail.com>
2023-10-29 01:13:18 +0500

    create equipment views

--- equipment/views.py
+++ equipment/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListAPIView
+from .models import EquipmentType, Equipment
+from .serializers import EquipmentTypeSerializer, EquipmentSerializer
 
-# Create your views here.
+
+class EquipmentTypeListAPIView(ListAPIView):
+    queryset = EquipmentType.objects.all()
+    serializer_class = EquipmentTypeSerializer
+
+
+class EquipmentViewSet(ModelViewSet):
+    queryset = Equipment.objects.all()
+    serializer_class = EquipmentSerializer
+
