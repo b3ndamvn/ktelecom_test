d71c775a995bf0a6b69bbe3fb5b2dcedff3cd1ce
Nikita Benda <nikitabenda69@gmail.com>
2023-10-29 01:14:50 +0500

    register equipment and equipmenttype to admin panel

--- equipment/admin.py
+++ equipment/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Equipment, EquipmentType
 
-# Register your models here.
+
+class EquipmentAdmin(admin.ModelAdmin):
+    pass
+
+
+class EquipmentTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Equipment, EquipmentAdmin)
+admin.site.register(EquipmentType, EquipmentTypeAdmin)
