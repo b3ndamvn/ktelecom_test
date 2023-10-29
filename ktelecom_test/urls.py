decb1e04bb23fc055966776c849282350536d4a4
Nikita Benda <nikitabenda69@gmail.com>
2023-10-29 23:52:20 +0500

    setup simplejwt authentication

--- ktelecom_test/urls.py
+++ ktelecom_test/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from .yasg import urlpatterns as doc_urls
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('equipment.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += doc_urls
