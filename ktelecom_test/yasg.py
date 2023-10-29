cb34b8c9a759ce4e012bd784b7ea93eed0063127
Nikita Benda <nikitabenda69@gmail.com>
2023-10-29 15:13:14 +0500

    update project title and description in documentation

--- ktelecom_test/yasg.py
+++ ktelecom_test/yasg.py
@@ -6,7 +6,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="ITS Test",
+      title="KTelecom Test",
       default_version='v1',
       description="Test description",
       license=openapi.License(name="BSD License"),
