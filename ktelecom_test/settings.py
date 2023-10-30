b76e0084cd30a957017be532496e4ab6fda5dbee
Nikita Benda <nikitabenda69@gmail.com>
2023-10-30 12:26:47 +0500

    update api/user/login route

--- ktelecom_test/settings.py
+++ ktelecom_test/settings.py
@@ -97,7 +97,6 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
 
--- ktelecom_test/urls.py
+++ ktelecom_test/urls.py
@@ -2,15 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from .yasg import urlpatterns as doc_urls
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+    TokenObtainSlidingView,
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('equipment.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/user/login/', TokenObtainSlidingView.as_view(), name='token_obtain_pair'),
 ]
 
 urlpatterns += doc_urls
